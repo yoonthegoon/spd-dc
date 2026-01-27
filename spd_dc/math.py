5a59d7f003bba27489bc1902e93d7b7ecbe01737
yunis <yunis@fedora>
2026-01-26 19:35:09 -0500

    add Pmf and Dud

---
+++ spd_dc/math.py
@@ -0,0 +1,121 @@
+from collections import defaultdict
+from math import sqrt
+from typing import Self
+
+
+class Pmf(defaultdict[int, float]):
+    """Probability Mass Function"""
+
+    def __init__(self, *args) -> None:
+        super().__init__(float)
+        if args:
+            self.update(*args)
+        self.normalize()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(mean={self.mean:.2f}, std_dev={self.std_dev:.2f})"
+
+    def __add__(self, other: "Pmf") -> "Pmf":
+        if not isinstance(other, Pmf):
+            raise TypeError(
+                f"unsupported operand type(s) for +: 'Pmf' and '{type(other)}'"
+            )
+        if not (self.is_normal and other.is_normal):
+            raise ValueError("cannot add non-normal distributions")
+
+        result = Pmf()
+
+        for x, px in self.items():
+            for y, py in reversed(other.items()):
+                result[x + y] += px * py
+
+        return result.normalize()
+
+    def __sub__(self, other: "Pmf") -> "Pmf":
+        if not isinstance(other, Pmf):
+            raise TypeError(
+                f"unsupported operand type(s) for -: 'Pmf' and '{type(other)}'"
+            )
+        if not (self.is_normal and other.is_normal):
+            raise ValueError("cannot add non-normal distributions")
+
+        result = Pmf()
+
+        for x, px in self.items():
+            for y, py in reversed(other.items()):
+                result[x - y] += px * py
+
+        return result.normalize()
+
+    def __mul__(self, other: float) -> "Pmf":
+        if not isinstance(other, float):
+            raise TypeError(
+                f"unsupported operand type(s) for *: 'Pmf' and '{type(other)}'"
+            )
+
+        result = self.copy()
+
+        for x in result:
+            result[x] *= other
+
+        return result
+
+    def clamped(self, *, a: int | None = None, b: int | None = None) -> "Pmf":
+        result = self.copy()
+        for x, p in self.items():
+            if a is not None and x < a:
+                result[a] += p
+                del result[x]
+            elif b is not None and x > b:
+                result[b] += p
+                del result[x]
+        return result
+
+    def copy(self) -> "Pmf":
+        return type(self)(self)
+
+    def normalize(self) -> Self:
+        total = self.total
+        for x in self:
+            self[x] /= total
+        return self
+
+    @property
+    def is_normal(self) -> bool:
+        return True if self.total == 1.0 else False
+
+    @property
+    def max(self) -> int:
+        return max(self)
+
+    @property
+    def mean(self) -> float:
+        return sum(x * p for x, p in self.items())
+
+    @property
+    def min(self) -> int:
+        return min(self)
+
+    @property
+    def std_dev(self) -> float:
+        return sqrt(self.variance)
+
+    @property
+    def total(self) -> float:
+        return sum(self.values())
+
+    @property
+    def variance(self) -> float:
+        total = self.total
+        mean = self.mean
+        return sum(p * (x - mean) ** 2 for x, p in self.items())
+
+
+class Dud(Pmf):
+    """Discrete Uniform Distribution"""
+
+    def __init__(self, a: int, b: int) -> None:
+        super().__init__({x: 1 for x in range(a, b + 1)})
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(a={self.min}, b={self.max})"
