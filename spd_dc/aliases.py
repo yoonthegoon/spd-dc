bb027ca9780009482197f6b112c89b0a7fe67b14
yunis <yunis@fedora>
2026-01-26 21:26:22 -0500

    add config

---
+++ spd_dc/aliases.py
@@ -0,0 +1,41 @@
+from typing import Literal
+
+ArmorAugment = Literal["defense", "evasion"] | None
+
+WeaponAugment = Literal["damage", "speed"] | None
+
+WeaponKind = Literal[
+    "worn shortsowrd",
+    "mage's staff",
+    "dagger",
+    "studded gloves",
+    "rapier",
+    "cudgel",
+    "shortsword",
+    "hand axe",
+    "spear",
+    "quarterstaff",
+    "dirk",
+    "sickle",
+    "pickaxe",
+    "sword",
+    "mace",
+    "scimitar",
+    "round shield",
+    "sai",
+    "whip",
+    "longsword",
+    "battle axe",
+    "flail",
+    "runic blade",
+    "assasin's blade",
+    "crossbox",
+    "katana",
+    "greatsword",
+    "war hammer",
+    "glaive",
+    "greataxe",
+    "greatshield",
+    "stone gauntlet",
+    "war scythe",
+]
---
+++ spd_dc/config.py
@@ -0,0 +1,38 @@
+import tomllib
+from pathlib import Path
+
+from pydantic import BaseModel, Field
+
+from spd_dc.aliases import ArmorAugment, WeaponAugment, WeaponKind
+
+ROOT_DIR = Path(__file__).parent.parent.absolute()
+
+
+class HeroConfig(BaseModel):
+    level: int = Field(default=1, ge=1, le=30)
+    strength: int = Field(default=10, ge=10, le=20)
+
+
+class WeaponConfig(BaseModel):
+    kind: WeaponKind
+    level: int = Field(default=0, ge=0)
+    augment: WeaponAugment = None
+    mastered: bool = False
+
+
+class ArmorConfig(BaseModel):
+    tier: int = Field(default=1, ge=1, le=5)
+    level: int = Field(default=0, ge=0)
+    augment: ArmorAugment = None
+    mastered = False
+
+
+class Config(BaseModel):
+    hero: HeroConfig = HeroConfig()
+    weapon: WeaponConfig | None = None
+    armor: ArmorConfig | None = None
+
+
+def get_config(file: Path = ROOT_DIR / "config.toml") -> Config:
+    with open(file, "rb") as f:
+        return Config(**tomllib.load(f))
